0596927382c8ae6a4182e0872a5e49870b977b38
Kevin <kevin.barr88@gmail.com>
2024-05-06 21:19:37 +0100

    fixes

--- client/src/Category.jsx
+++ client/src/Category.jsx
@@ -1,47 +1,41 @@
+import "./css/common.css";
+import "./css/core.css";
+import "./css/bodyMain.css";
+import ALink from "./ALink";
 
-import "./css/common.css"
-import "./css/core.css"
-import "./css/bodyMain.css"
-import AnchorLink from "./ALink";
+export default function Category() {
+  const htmlFilters = metaFilters.map((filter) => (
+    <ALink
+      key={filter.innerText}
+      url={filter.url}
+      ariaLabel={filter.ariaLabel}
+      innerText={filter.innerText}
+    />
+  ));
 
-export default function Category()
-{
-    const htmlFilters = metaFilters.map((filter) => (
-        <AnchorLink
-          key={filter.innerText}
-          url={filter.url}
-          ariaLabel={filter.ariaLabel}
-          innerText={filter.innerText}
-        />
-      ));
-
-    return (
-    <div id="tab-div">
-        {htmlFilters}
-  </div>
-    )
+  return <div id="tab-div">{htmlFilters}</div>;
 }
 
 // Storage array - could be a JSON to make it pure data too.
 const metaFilters = [
-    {
-        url: "/all",
-        ariaLabel: "No filter. Get all posts.",
-        innerText: "ALL"
-    },
-    {
-        url: "/new",
-        ariaLabel: "Filter by latest posts",
-        innerText: "NEW"
-    },
-    {
-        url: "/popular",
-        ariaLabel: "Filter by most likes",
-        innerText: "POPULAR"
-    },
-    {
-        url: "/tag",
-        ariaLabel: "Filter by tags",
-        innerText: "BY TAG"
-    },
-]
+  {
+    url: "/all",
+    ariaLabel: "No filter. Get all posts.",
+    innerText: "ALL",
+  },
+  {
+    url: "/new",
+    ariaLabel: "Filter by latest posts",
+    innerText: "NEW",
+  },
+  {
+    url: "/popular",
+    ariaLabel: "Filter by most likes",
+    innerText: "POPULAR",
+  },
+  {
+    url: "/tag",
+    ariaLabel: "Filter by tags",
+    innerText: "BY TAG",
+  },
+];
